f25aa92e5405349c7696117208020fdef8364a29
bliareg <bliareg3@gmail.comm>
2019-05-05 11:33:25 +0300

    build(package): Add history

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, Router } from 'react-router-dom'
 import { SkypeScreen, InterviewScreen } from 'Screens/index';
 import { ROUTES } from 'constants/index';
+import { history } from 'utils';
 
 class Routes extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <Route path={ROUTES.skypeIntroductionScreen} exact component={SkypeScreen} />
         <Route path={ROUTES.chat_interview} exact component={InterviewScreen} />
-      </BrowserRouter>
+      </Router>
     );
   }
 }
