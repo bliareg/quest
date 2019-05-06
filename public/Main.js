27a1c0e10e82baa19146e3b5ecbeb51714f0efff
bliareg <bliareg3@gmail.comm>
2019-05-06 22:12:34 +0300

    fix(Main.js): Configure electron window to work in fullscreen

--- public/Main.js
+++ public/Main.js
@@ -5,7 +5,8 @@ function createWindow() {
   // Create the browser window.
   var win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    fullscreen: true
   })
 
   // win.loadURL('http://localhost:3000/');
