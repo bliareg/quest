e709f310d84adb4ba15af7738620a21ced4f28c6
anizheg <etecture.nizhegorodov@extaccount.com>
2019-05-24 19:07:29 +0300

    complete chat

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,4 +1,4 @@
-import final1Img from './final1.png';
+import final1Img from './final1.jpg';
 import final2Img from './final2.png';
 import skype from './skype.png';
 import skypeLogo from './skype-logo.png';
--- src/components/Audio/index.tsx
+++ src/components/Audio/index.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import { SkypeSound } from 'assets/audio/';
 
-const AudioPlayer = () => {
-    return (
-        <audio
-            id="audio_player"
-            autoPlay
-            loop
-            src={SkypeSound}
-        />
-    )
-};
+class AudioPlayer extends React.Component<{}> {
+    public refs: any;
+
+    componentDidMount(): void {
+        this.refs.audio.load();
+        this.refs.audio.play();
+    }
+
+    render(){
+        return (
+            <audio
+                ref="audio"
+                id="audio_player"
+                autoPlay
+                loop
+                src={SkypeSound}
+            />
+        )
+    }
+}
 
 export default AudioPlayer;
